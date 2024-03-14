8dc4580ca99d0c16e4945b52352cfec2017d1d24
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2024-03-14 12:18:16 +0800

    Update allow.sh

--- allow.sh
+++ allow.sh
@@ -11,98 +11,93 @@ fi
 
 # 提取SSH端口
 SSH_PORT=$(grep -oP "(?<=Port\s)\d+" /etc/ssh/sshd_config)
+SSH_PORT=${SSH_PORT:-22}  # 如果没有提取到端口，默认为22
 
-# 检查是否成功提取端口
-if [ -z "$SSH_PORT" ]; then
-    echo "无法提取SSH端口。请检查sshd_config文件。默认使用端口22。"
-    SSH_PORT=22
-else
-    echo "当前SSH端口为: $SSH_PORT"
-fi
+# 输出SSH端口
+echo "当前SSH端口为: $SSH_PORT"
 
-# 函数：解析域名为IP
-resolve_domain() {
-    local domain="$1"
-    local result
-    result=$(dig +short "$domain")
+# 读取A.json文件中的域名列表和端口列表
+readarray -t domains < <(jq -r '.domain[]' "$json_file")
+readarray -t ports < <(jq -r '.port[]' "$json_file")
+ports+=("$SSH_PORT")  # 将SSH端口加入到ports数组中
 
-    # 检查是否成功解析域名
-    if [ -z "$result" ]; then
-        echo "错误: 无法解析域名 $domain。"
-        return 1
-    else
-        echo "$result"
-    fi
-}
-
-# 函数：添加UFW规则
-add_ufw_rule() {
-    local ip="$1"
-    local port="$2"
-    local rule="allow from $ip to any port $port"
-
-    # 检查规则是否已存在
-    if ! ufw status | grep -F -q -- "$rule"; then
-        ufw allow from "$ip" to any port "$port"
-        echo "添加规则：$rule"
-    else
-        echo "规则已存在：$rule"
-    fi
-}
-
-# 函数：删除UFW规则
-delete_ufw_rule() {
-    local port="$1"
-
-    # 获取UFW规则中允许的IP列表
-    local ufw_status=$(grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" /etc/ufw/user.rules | sort -u)
-    
-    # 输出UFW规则中允许的IP
-    echo "UFW规则中允许的IP（$SSH_PORT端口）："
-    for allowed_ip in ${ufw_status[@]}; do
-        echo "$allowed_ip"
-    done
-
-    # 从A.json中提取的IP
-    local domains=($(jq -r '.domain[]' "$json_file"))
-    local resolved_ips=()
-    for domain in "${domains[@]}"; do
-        ip=$(resolve_domain "$domain")
-
-        # 检查是否成功解析域名
-        if [ $? -eq 0 ]; then
-            resolved_ips+=("$ip")
-        fi
-    done
-
-    # 执行UFW删除多余的规则
-    for existing_ip in ${ufw_status[@]}; do
-        # 如果该IP不在解析后的IP列表中，删除规则
-        if [[ ! " ${resolved_ips[@]} " =~ " ${existing_ip} " ]]; then
-            ufw delete allow from "$existing_ip" to any port "$port"
-            echo "删除规则：allow from $existing_ip to any port $port"
-        fi
-    done
-}
-
-# 读取A.json文件
-domains=($(jq -r '.domain[]' "$json_file"))
-
-# 存储解析后的IP
-resolved_ips=()
+# 解析域名并存储IP地址
+ips=()
 for domain in "${domains[@]}"; do
-    ip=$(resolve_domain "$domain")
-
-    # 检查是否成功解析域名
-    if [ $? -eq 0 ]; then
-        resolved_ips+=("$ip")
-    fi
+    ip=$(dig +short "$domain" | head -n1)
+    [ -n "$ip" ] && ips+=("$ip") || echo "无法解析域名 $domain"
 done
 
-# 执行UFW操作
-for ip in "${resolved_ips[@]}"; do
-    add_ufw_rule "$ip" "$SSH_PORT"
+# 在user.rules中查找与SSH端口对应的IP
+ufwips=($(grep -oP "allow\s+any\s+$SSH_PORT\s+0\.0\.0\.0/0\s+any\s+\K\S+" /etc/ufw/user.rules))
+
+# 打印IP地址和端口
+echo "IP地址: ${ips[*]}"
+echo "端口: ${ports[*]}"
+
+# 计算差异IP地址
+ips_diff=()
+ufwips_diff=()
+
+# 计算ips中减去ufwips的差集
+ips_diff=($(comm -13 <(printf "%s\n" "${ufwips[@]}" | sort) <(printf "%s\n" "${ips[@]}" | sort)))
+
+# 计算ufwips中减去ips的差集
+ufwips_diff=($(comm -13 <(printf "%s\n" "${ips[@]}" | sort) <(printf "%s\n" "${ufwips[@]}" | sort)))
+
+# 输出结果
+echo "ips 减去 (ips 和 ufwips 的交集) = ${ips_diff[*]}"
+echo "ufwips 减去 (ips 和 ufwips 的交集) = ${ufwips_diff[*]}"
+
+# 函数：管理UFW规则
+ufw_rule() {
+    local action="$1"
+    local ip="$2"
+    local port="$3"
+    local rule="allow from $ip to any port $port"
+    case "$ip" in
+        "0.0.0.0/0")
+            case "$action" in
+                "allow")
+                    ufw allow "$port" > /dev/null && echo "已添加规则：allow any port $port"
+                    ;;
+                "delete")
+                    ufw delete allow "$port" > /dev/null && echo "已删除规则：allow any port $port"
+                    ;;
+                *)
+                    echo "无效的操作：$action"
+                    return 1
+                    ;;
+            esac
+            ;;
+        *)
+            case "$action" in
+                "allow")
+                    ufw allow $rule > /dev/null && echo "已添加规则：$rule"
+                    ;;
+                "delete")
+                    ufw delete $rule > /dev/null && echo "已删除规则：$rule"
+                    ;;
+                *)
+                    echo "无效的操作：$action"
+                    return 1
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+
+# 添加ips_diff到ports
+for ip in "${ips_diff[@]}"; do
+    for port in "${ports[@]}"; do
+        ufw_rule "allow" "$ip" "$port"
+    done
 done
 
-# 删除多余的UFW规则
-delete_ufw_rule "$SSH_PORT"
+# 删除ufwips_diff到ports
+for ip in "${ufwips_diff[@]}"; do
+    for port in "${ports[@]}"; do
+        ufw_rule "delete" "$ip" "$port"
+    done
+done
